2c914a5d1037b8b085723fb61a8d4790ad4b79ae
Doc011997 <50246766+Doc011997@users.noreply.github.com>
2022-05-14 20:55:21 +0200

    Add files via upload

--- client/components/Login.vue
+++ client/components/Login.vue
@@ -1,83 +1,141 @@
 <template>
-  <div id="login">
-      <form v-on:submit.prevent="login">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Adresse email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                 placeholder="Entrez l'Email" v-model="credential.email" v-bind:class="{'is-invalid': invaliddata}"
-                 required>
-        </div>
-        <div class="form-group">
-          <label for="inputPassword">Mot de Passe</label>
-          <input type="password" class="form-control" id="inputPassword" placeholder="Mot de Passe"
-                 v-model="credential.password" v-bind:class="{'is-invalid': invaliddata}" required>
-          <div class="invalid-feedback" v-if="invaliddata === true">
-            Le nom d'utilisateur ou le mot de passe sont incorrects
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Se Connecter</button>
 
-      </form>
+<header>
+  <h1>EFREI LIBRARY</h1>
+  <h2>Welcome to your library</h2>
+  </header>
+
+  <div class="card">
+    <h1 class="card__title" v-if="mode == 'login'">Connexion</h1>
+    <h1 class="card__title" v-else>Inscription</h1>
+    <p class="card__subtitle" v-if="mode == 'login'">Tu n'as pas encore de compte ? <span class="card__action" @click="switchToCreateAccount()">Créer un compte</span></p>
+    <p class="card__subtitle" v-else>Tu as déjà un compte ? <span class="card__action" @click="switchToLogin()">Se connecter</span></p>
+    <div class="form-row">
+      <input v-model="email" class="form-row__input" type="text" placeholder="Adresse mail"/>
+    </div>
+    <div class="form-row" v-if="mode == 'create'">
+      <input v-model="prenom" class="form-row__input" type="text" placeholder="Prénom"/>
+      <input v-model="nom" class="form-row__input" type="text" placeholder="Nom"/>
+    </div>
+    <div class="form-row">
+      <input v-model="password" class="form-row__input" type="password" placeholder="Mot de passe"/>
+    </div>
+    <div class="form-row" v-if="mode == 'login' && status == 'error_login'">
+      Adresse mail et/ou mot de passe invalide
+    </div>
+    <div class="form-row" v-if="mode == 'create' && status == 'error_create'">
+      Adresse mail déjà utilisée
+    </div>
+    <div class="form-row">
+      <button @click="login()" class="button" :class="{'button--disabled' : !validatedFields}" v-if="mode == 'login'">
+        <span v-if="status == 'loading'">Connexion en cours...</span>
+        <span v-else>Connexion</span>
+      </button>
+      <button @click="createAccount()" class="button" :class="{'button--disabled' : !validatedFields}" v-else>
+        <span v-if="status == 'loading'">Création en cours...</span>
+        <span v-else>Créer mon compte</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-module.exports = {
-  props: {
-    invaliddata: {type: Boolean, default: false}
-  },
-  data() {
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Login',
+  data: function () {
     return {
-      credential: {email: '', password: ''}
+      mode: 'login',
+      email: '',
+      prenom: '',
+      nom: '',
+      password: '',
     }
+  },
+  mounted: function () {
+    if (this.$store.state.user.userId != -1) {
+      this.$router.push('/profile');
+      return ;
+    }
+  },
+  computed: {
+    validatedFields: function () {
+      if (this.mode == 'create') {
+        if (this.email != "" && this.prenom != "" && this.nom != "" && this.password != "") {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        if (this.email != "" && this.password != "") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    ...mapState(['status'])
   },
   methods: {
-    login() {
-      this.$emit('login', this.credential)
-    }
+    switchToCreateAccount: function () {
+      this.mode = 'create';
+    },
+    switchToLogin: function () {
+      this.mode = 'login';
+    },
+    login: function () {
+      const self = this;
+      this.$store.dispatch('login', {
+        email: this.email,
+        password: this.password,
+      }).then(function () {
+        self.$router.push('/profile');
+      }, function (error) {
+        console.log(error);
+      })
+    },
+    createAccount: function () {
+      const self = this;
+      this.$store.dispatch('createAccount', {
+        email: this.email,
+        nom: this.nom,
+        prenom: this.prenom,
+        password: this.password,
+      }).then(function () {
+        self.login();
+      }, function (error) {
+        console.log(error);
+      })
+    },
   }
 }
 </script>
 
 <style scoped>
-#login {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  display: flex;
-}
-
-#box {
-  display: block;
-  margin: auto;
-  min-width: 30%;
-  padding: 15px;
-}
-
-form .form-group label {
-  padding-top: 10px;
-}
-
-@media (min-width: 700px) {
-  #box {
-    border: 1px solid black;
-    border-radius: 15px;
+  .form-row {
+    display: flex;
+    margin: 16px 0px;
+    gap:16px;
+    flex-wrap: wrap;
   }
-}
 
-h1, h2 {
-  text-align: center;
-}
+  .form-row__input {
+    padding:8px;
+    border: none;
+    border-radius: 8px;
+    background:#f2f2f2;
+    font-weight: 500;
+    font-size: 16px;
+    flex:1;
+    min-width: 100px;
+    color: black;
+  }
 
-h1 {
-  font-size: 40px;
-}
+  .form-row__input::placeholder {
+    color:#aaaaaa;
+  }
 
-h2 {
-  font-size: 18px;
-}
 
-form button {
-  margin-top: 10px;
-}
-</style>
+</style>>
